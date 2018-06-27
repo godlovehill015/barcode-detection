7c4bd5dd854a84b09b59422305575a358d255291
hill <godlovehill@foxmail.com>
2018-06-27 16:58:49 +0800

    add io process

--- detect_barcode.py
+++ detect_barcode.py
@@ -7,10 +7,16 @@
 # import the necessary packages
 import numpy as np
 import argparse
+import os
 import cv2
 
+f = open('data.csv', 'w')
+for i in range(0, 10):
+    f.write(str(i*0.1))
+    f.write('\n')   #逐行将数据写入到文本
+
 # load the image and convert it to grayscale
-image = cv2.imread("/home/pi/PycharmProjects/file/barcode_01.jpg")
+image = cv2.imread("/home/pi/Desktop/100/barcode-detection/barcode_01.jpg")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # compute the Scharr gradient magnitude representation of the images
@@ -21,12 +27,15 @@ gradY = cv2.Sobel(gray, ddepth = cv2.cv.CV_32F, dx = 0, dy = 1, ksize = -1)
 # subtract the y-gradient from the x-gradient
 gradient = cv2.subtract(gradX, gradY)
 gradient = cv2.convertScaleAbs(gradient)
-# 显示梯度图像
+
 cv2.imshow("gradient", gradient)
 
 # blur and threshold the image
 blurred = cv2.blur(gradient, (9, 9))
+cv2.imshow("blur", gradient)
+
 (_, thresh) = cv2.threshold(blurred, 225, 255, cv2.THRESH_BINARY)
+cv2.imshow("threshold", gradient)
 
 # construct a closing kernel and apply it to the thresholded image
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 7))
